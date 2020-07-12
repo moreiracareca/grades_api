1938b7da2aa7a814c2876332c9ab29c15d51a215
Ze_Moreira <Careca@NB-CARECA>
2020-07-12 11:35:13 -0300

    change cors

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import cors from 'cors';
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
 
-import { gradeRouter } from './routes/gradeRouter.js';
-import { logger } from './config/logger.js';
-import { db } from './models/index.js';
+import { gradeRouter } from "./routes/gradeRouter.js";
+import { logger } from "./config/logger.js";
+import { db } from "./models/index.js";
 
 (async () => {
   try {
@@ -12,7 +12,7 @@ import { db } from './models/index.js';
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-    logger.info('Conectado ao banco de dados');
+    logger.info("Conectado ao banco de dados");
   } catch (error) {
     logger.error(`Erro ao conectar no banco de dados! ${error}`);
 
@@ -25,16 +25,12 @@ const app = express();
 //define o dominio de origem para consumo do servico
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(
-  cors({
-    origin: 'http://localhost:8080',
-  })
-);
+app.use(cors());
 
 app.use(gradeRouter);
 
-app.get('/', (req, res) => {
-  res.send('API em execucao');
+app.get("/", (req, res) => {
+  res.send("API em execucao");
 });
 
 app.listen(process.env.PORT || 8081, () => {
